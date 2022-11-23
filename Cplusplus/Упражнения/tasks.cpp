aeac12d97482160108530daec0d53eb8b3b4aa0a
NorthernBlow <rheinhart@yandex.ru>
2022-11-23 05:12:01 +0300

    Hell world C PLUS PLUS

---
+++ Cplusplus/Упражнения/tasks.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+// Программы на С++ начинается с выполнения функции main
+int main() {   
+
+cout << "Hello, world!\n"; // cout -- character output stream -- поток вывода символов
+return 0;
+
+} 
