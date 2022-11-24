ff95b9a2834b073b16ba943a583975715e02b288
NorthernBlow <rheinhart@yandex.ru>
2022-11-25 00:43:16 +0300

    segfault

---
+++ Cplusplus/Упражнения/segfault.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main() {
+
+	int x = 10;
+	int16_t A [10];
+	int y = 3;
+
+
+	A[10] = 1;
+
+	std::cout << x << ' ' << y << '\n';
+
+
+	return 0;
+}
---
+++ Cplusplus/Упражнения/task33.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main () {
+
+int var1;
+int var2;
+int var3;
+
+cout << "Введите 3 целых числа";
+
+cin >> var1 >> var2 >> var3;
+
+if (var1 > var2)
+	cout << var1;
+else 
+	cout << var2;
+
+
+	return 0;
+}
