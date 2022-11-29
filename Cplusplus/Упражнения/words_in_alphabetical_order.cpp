5d98f0afc385d5697bfcb102a357e73cf4b28ca7
NorthernBlow <rheinhart@yandex.ru>
2022-11-30 00:57:49 +0300

    denied filter

--- Cplusplus/Упражнения/words_in_alphabetical_order.cpp
+++ Cplusplus/Упражнения/words_in_alphabetical_order.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
 #include "std_lib_facilities.h"
-using namespace std;
+#include <vector>
 
 int main()
 {
 	vector<string> words;
+	vector<string> purgewords = {"windows","макбук","фашисты","яваскрипт","ангуляр","реакт","нода","продакшн","прод","энтерпрайз"};
 
 	for (string temp; cin >> temp;) // чтение слов, разделенных пробелом и запись в вектор типа string
 		words.push_back(temp); // собственно запись в вектор
@@ -13,9 +13,16 @@ int main()
 
 	sort(words); // сортировка слов
 
-	for (int i = 0; i<words.size(); ++i)
+	for (int i = 0; i<words.size(); ++i)   // удаляет повторяющиеся слова
 		if (i==0 || words[i-1] != words[i])
 			cout << words[i] << '\n';
+
+	// выводит отдельно нежелательные слова
+
+	for (int i = 0; i<purgewords.size(); ++i)
+		for (int j=0; j<words.size(); ++j)
+			if (purgewords[i] == words[j])
+				cout << "Запрещенные слова: " << purgewords[i] << '\n';
 
 // Ctrl + D для остановки ввода данных
 
