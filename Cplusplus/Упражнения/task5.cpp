ee22f91d65ead42f5beba9071e2344ad6d7ee9df
NorthernBlow <rheinhart@yandex.ru>
2022-12-01 00:33:55 +0300

    while

---
+++ Cplusplus/Упражнения/task5.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+
+int main()
+{
+
+	char symbol;
+
+	symbol = '_';
+	int a = 0;
+
+	while(a < 100){
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		std::cout << symbol << std::endl;
+		a++;
+	}
+
+
+
+
+
+
+
+
+
+
+}
