694147d42028f63789e1d8670e0c1f006f0dec60
NorthernBlow <rheinhart@yandex.ru>
2023-01-12 09:55:08 +0300

    slices

--- golang/hello.go
+++ golang/hello.go
@@ -1,8 +1,32 @@
 package main
+import "fmt"
 
 
 func main() {
-	println("Hello golang ((:")
 
+	var slice []int
+
+	fmt.Println(len(slice))
+	slices()
+	return
+	
 
 }
+
+
+func slices() {
+
+	slice := []int{
+		10,
+		12,
+	}
+
+	
+	fmt.Println("Это капасити->", cap(slice), "\n", "Это длинна->", len(slice))
+	var slice2 []int = append(slice, 13, 14, 16)
+	fmt.Println("Это капасити->", cap(slice2), "\n", "Это длинна->", len(slice2), slice2)
+
+	return
+}
+
+
