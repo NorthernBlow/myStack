8597442de98cc2213cf8c498368234df90a224dd
NorthernBlow <rheinhart@yandex.ru>
2023-06-04 18:42:22 +0300

    Продолжаю делать упр. к третьей главе Лутца

--- python/learn/exercise.py
+++ python/learn/exercise.py
@@ -1,16 +1,69 @@
+import functools
+
+
 S: str = 'cycle of death'
 sum: int = 0
 ascii_list: list = []
 
 for i in S:
     ord(i)
-    sum += ord(i) + ord(i)
     if ord(i) not in ascii_list:
         ascii_list.append(ord(i))
         #print(ascii_list)
 
-print(ascii_list, 'это асци лист')
+#print(ascii_list, 'это асци лист')
 
-mmmap = map(ord, S)
+#mmmap = map(ord, S)
 
-print(mmmap)
+
+#for code in map(ord, S):
+#    print(code)
+
+
+
+#why = [code for code in map(ord, S)]
+#print(why)
+
+
+#HASH: dict = {'name': 'goremyka', 'lvl': 29, 'race': 'elly'}
+
+
+
+#for key in HASH.keys():
+#    sorted_list = []
+#    sorted_list.append(key)
+#    sorted_list.sort()
+#   # print(sorted_list)
+
+
+
+#sorted_list = list(filter(sorted, HASH))
+#print(sorted_list)
+
+
+
+L = [1, 2, 3, 4, 5, 6, 7, 16, 32, 64]
+X = 5
+found = False
+
+i = 0
+
+while not found and i < len(L):
+    if 2 ** X == L[i]:
+        found = 1
+    else:
+        i += 1
+
+
+    if found:
+        print('at index', i)
+
+    else:
+        print(X, 'not found')
+
+
+
+
+
+
+
